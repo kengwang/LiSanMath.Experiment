ac87aa51aec921e464dc0949cda4570f4e7d8484
Kengwang <github@kengwang.com.cn>
2024-10-09 10:53:45 +0800

    add: 数值计算方法 - 牛顿法

---
+++ NumCal.App/NewtonMethod.cs
@@ -0,0 +1,25 @@
+﻿using Spectre.Console;
+using Function = System.Func<double, double>;
+
+namespace NumCal.App;
+
+public static class NewtonMethod
+{
+    public static double Solve(Function f, Function df, double x0, double tol = 1e-6, int maxIter = 100, bool debug = false)
+    {
+
+        var x = x0;
+        AnsiConsole.MarkupLine("选取的初值: [bold blue]{0}[/]", x);
+        for (var i = 0; i < maxIter; i++)
+        {
+            var dx = -f(x) / df(x);
+            x += dx;
+            AnsiConsole.MarkupLine("迭代的 x{0} = [bold blue]{1}[/]", i,x);
+            if (Math.Abs(dx) < tol)
+            {
+                return x;
+            }
+        }
+        throw new Exception("Newton's method did not converge");
+    }
+}
---
+++ NumCal.App/Program.cs
@@ -0,0 +1,10 @@
+﻿using NumCal.App;
+using Spectre.Console;
+
+var result = NewtonMethod.Solve(
+    (x) => x*x - Math.Exp(x),
+    (x) => 2*x - Math.Exp(x),
+    0.0,
+    double.Epsilon
+    );
+    AnsiConsole.MarkupLine("得到的解为: [bold green]{0}[/]", result);
